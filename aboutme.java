9b11dccb7b08aa31daf3504cb0cb768b2dd5cd3b
Cindy Tung <cindytung@berkeley.edu>
2015-06-09 19:19:38 -0700

    update

---
+++ aboutme.java
@@ -0,0 +1,11 @@
+import aboutme;
+import java.util.*;
+
+String name = "Cindy Tung";
+
+public void education() {
+	ArrayList<String> edu = new ArrayList<String>;
+	edu.add("School: University of California, Berkeley");
+	edu.add("Major: Computer Science");
+	edu.add("Year: 2014-18");
+}
